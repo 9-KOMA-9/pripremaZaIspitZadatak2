7e6c19e179cba2f9037a52542793492b1da17127
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-08 08:50:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaZaIspitZadatak2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prijestupne godine u razdoblju 1900 - 2100");
+            for (int i = 1900; i <= 2100; i++)
+            {
+                if ((i % 400 == 0) || (i % 4 ==0) && (i%100 !=0))
+                {
+                   
+                    Console.WriteLine("prijestupna godina je "+ i);
+                }
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
